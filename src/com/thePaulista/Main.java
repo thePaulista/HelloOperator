1a51c2d0afa38d8d4c47f58ed6a2d41844696ee4
Hedy <hedywoo@gmail.com>
2016-07-08 18:21:15 -0500

    Return dynamic value in the calc score method

--- src/com/thePaulista/Main.java
+++ src/com/thePaulista/Main.java
@@ -6,14 +6,18 @@ public class Main {
         calculateScore(true, 800, 5, 100);
 
         calculateScore(true, 100, 2, 3);
+
+        calculateScore(false, 1, 1, 1);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("your final score was "  + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
     }
 }
